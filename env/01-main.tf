57d5799656c0eb98ca9a1930a39c0b61c5a96107
simon mbugua <smbugua11@gmail.com>
2023-03-10 22:42:42 +0300

    add more terraform variables and configurations

--- env/01-main.tf
+++ env/01-main.tf
@@ -38,4 +38,5 @@ provider "google-beta" {
    gke_machine_type = var.gke_machine_type
    db_password = var.db_password
    primary_region = var.primary_region
+   min_gke_master_version = var.min_gke_master_version
  }
